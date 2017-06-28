7a466cd488414aeac83f2b6468177ef1efda139e
cocosar07 <cocosar07370@gmail.com>
2017-06-28 22:12:23 +0200

    Basic map

--- game.lua
+++ game.lua
@@ -1,17 +1,21 @@
 require "player"
+require "map"
 
 Game = {}
 
 function Game:init()
   love.window.setTitle("Project_S - Game")
 
+  self.map = Map()
   self.player = Player()
 end
 
 function Game:update(dt)
+  self.map:update(dt)
   self.player:update(dt)
 end
 
 function Game:draw()
+  self.map:draw()
   self.player:draw()
 end
--- map.lua
+++ map.lua
@@ -1,1 +1,47 @@
---TODO : Everything
+Map = Entity:extend()
+
+function Map:new()
+  self.super:new()
+
+  self:init("maps/map0.lua")
+end
+
+function Map:init(filename)
+  local levelTmp = love.filesystem.load(filename)()
+
+  self.grid = levelTmp.grid
+
+  self.height = #self.grid
+  self.width = #self.grid[1]
+
+  self.tileWidth, self.tileHeight = love.graphics.getWidth()/self.width, love.graphics.getHeight()/self.height
+end
+
+function Map:update(dt)
+  self.super:update(dt)
+end
+
+function Map:draw()
+  self.super:draw()
+
+  for x=1,self.width do
+    for y=1,self.height do
+      if self:isSolid(x, y) then
+        love.graphics.setColor(0, 0, 0)
+      else
+        love.graphics.setColor(50, 50, 50)
+      end
+
+      love.graphics.rectangle("fill", (x-1) * self.tileWidth, (y-1) * self.tileHeight, self.tileWidth, self.tileHeight)
+    end
+  end
+end
+
+function Map:isSolid(x, y)
+  x, y = math.floor(x), math.floor(y)
+  if x < 1 or x > self.width or y < 1 or y > self.height then
+    return true
+  end
+
+  return self.grid[y][x] == 1
+end
