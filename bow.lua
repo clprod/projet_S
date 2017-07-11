019fd7679d0525a407ab119c18527a9bd1e94c52
cocosar07 <cocosar07370@gmail.com>
2017-07-11 20:31:43 +0200

    Arrow animations

--- bow.lua
+++ bow.lua
@@ -30,7 +30,7 @@ function Bow:update(dt)
 
 	for i=#self.firedArrows,1,-1 do
 		self.firedArrows[i]:update(dt)
-		if self.firedArrows[i]:isColliding() then
+		if self.firedArrows[i]:canBeDestroyed() then
 			table.remove(self.firedArrows, i)
 		end
 	end
@@ -69,10 +69,13 @@ function Bow:shoot()
 end
 
 -- ---------------- Arrow -----------------------
+local MOVING, HIT = 1, 2
 
 local arrowImage = love.graphics.newImage("ressources/arrow.png")
 local frameWidth, frameHeight = 32, 32
 local frameNumber = 6
+local movingAnimationSpeed = 1/24
+local hitAnimationSpeed = 1/12
 
 Arrow = Ammo:extend()
 
@@ -86,27 +89,99 @@ function Arrow:new(game, initialPosition, mousePos, power, isAlly)
 	self.width = 32
 	self.height = 32
 
-  self.frames = {}
+  self.hitEntity = nil
+  self.positionOffset = nil
 
-  for i=0,frameNumber-1 do
-    table.insert(self.frames, love.graphics.newQuad(i * self.width, 0, frameWidth, frameHeight, arrowImage:getWidth(), arrowImage:getHeight()))
+  self.frames = {}
+  self.currentState = MOVING
+  self.currentFrame = 1
+  self.lastFrameChange = 0
+  self.animationSpeed = movingAnimationSpeed
+
+  for i=MOVING,HIT do
+    self.frames[i] = {}
+    for j=0,frameNumber-1 do
+      table.insert(self.frames[i], love.graphics.newQuad(j * frameWidth, (i-1) * frameHeight, frameWidth, frameHeight, arrowImage:getWidth(), arrowImage:getHeight()))
+    end
   end
 end
 
 function Arrow:update(dt)
 	Arrow.super.update(self, dt)
 	self:move(dt)
+
+  if self.currentState == MOVING and self:isColliding() then
+    self.currentState = HIT
+    self.currentFrame = 1
+    self.animationSpeed = hitAnimationSpeed
+    self.lastFrameChange = 0
+  end
+
+  -- Animation
+  self.lastFrameChange = self.lastFrameChange + dt
+
+  if self.lastFrameChange >= self.animationSpeed then
+    self.currentFrame = self.currentFrame + 1
+    if self.currentFrame > frameNumber then
+      if  self.currentState == MOVNG then
+        self.currentFrame = 1
+      else
+        self.currentFrame = frameNumber
+      end
+    end
+
+    self.lastFrameChange = 0
+  end
 end
 
 function Arrow:draw()
 	Arrow.super.draw(self)
 
 	love.graphics.setColor(255, 255, 255)
-  love.graphics.draw(arrowImage, self.frames[1], self.position.x, self.position.y, self.velocity:angleTo(), 1, 1, frameWidth/2, frameHeight/2)
+  love.graphics.draw(arrowImage, self.frames[self.currentState][self.currentFrame], self.position.x, self.position.y, self.velocity:angleTo(), 1, 1, frameWidth/2, frameHeight/2)
 end
 
 function Arrow:move(dt)
-  self.velocity.y = self.velocity.y + gravity * dt
+  if self.currentState == MOVING then
+    self.velocity.y = self.velocity.y + gravity * dt
+    self.position = self.position + self.velocity
+  else
+    if self.hitEntity ~= nil then
+      self.position = self.hitEntity.position - self.positionOffset
+    end
+  end
+end
 
-  self.position = self.position + self.velocity
+function Arrow:isColliding()
+  local headPosition = self.position + self.velocity:normalized() * self.width/2
+
+  -- Map collision
+  if self.game.map:isPixelPosSolid(headPosition) then
+		return true
+	end
+
+	-- enemy collision
+	for i,entity in ipairs(self.game.entities) do
+		if self.isAlly and entity:is(Enemy) then
+			if entity:isPositionColliding(headPosition) then
+        self.hitEntity = entity
+        self.positionOffset = entity.position - self.position
+				entity:getDamaged(1)
+			  return true
+			end
+		elseif not self.isAlly and entity:is(Player) then
+			if entity:isPositionColliding(headPosition) then
+        self.hitEntity = entity
+        self.positionOffset = entity.position - self.position
+				entity:getDamaged(1)
+			  return true
+			end
+		end
+	end
+
+	return false
+end
+
+function Arrow:canBeDestroyed()
+  return self.currentState == HIT and self.currentFrame == frameNumber
 end
